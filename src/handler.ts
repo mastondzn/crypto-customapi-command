badba65e99176cedb6a2f38ab4c27d61e5515dd4
MastonDZN <mastondzn@gmail.com>
2022-03-18 06:00:35 +0200

    change number of coins fetched per page

--- src/handler.ts
+++ src/handler.ts
@@ -3,7 +3,7 @@ import type { Coin, FullCoin } from './types';
 const baseUrl = 'https://api.coingecko.com/';
 const path = 'api/v3/coins/markets';
 const urlParams =
-  '?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h,24h,7d';
+  '?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h,24h,7d';
 
 const url = `${baseUrl}${path}${urlParams}`;
 const upArrow = '↑';
